2f55b25f9d6693003adf643055e2577436dcd093
GORIPARTHI YAGNESWAR <gyagneswar87@gmail.com>
2026-08-18 15:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google import genai
 from PIL import Image
+import requests
 
 st.set_page_config(
     page_title="Agri-Vision Agent",
@@ -16,6 +17,8 @@ uploaded_file = st.file_uploader(
     type=["jpg", "jpeg", "png"]
 )
 
+city = st.text_input("📍 Enter your farm location", "Guntur")
+
 if uploaded_file:
     image = Image.open(uploaded_file)
 
@@ -23,14 +26,16 @@ if uploaded_file:
 
     if st.button("🔍 Analyze Crop", use_container_width=True):
 
-        client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
+        client = genai.Client(
+            api_key=st.secrets["GEMINI_API_KEY"]
+        )
 
         prompt = """
 You are the Vision Agent of an agricultural AI system.
 
-Analyze the uploaded crop image.
+Analyze this crop image.
 
-Give the answer in this format:
+Return:
 
 Crop:
 Visible symptoms:
@@ -39,17 +44,48 @@ Confidence:
 Why:
 What should be checked next:
 
-Important:
 Do not claim that an image alone proves a disease.
-If the image is unclear, say that the diagnosis is uncertain.
+If the image is unclear, say the diagnosis is uncertain.
 Do not recommend a specific pesticide or chemical from the image alone.
 """
 
-        with st.spinner("🤖 Vision Agent is analyzing..."):
+        with st.spinner("👁️ Vision Agent analyzing..."):
             response = client.models.generate_content(
                 model="gemini-3.6-flash",
                 contents=[prompt, image]
             )
 
-        st.subheader("🤖 Vision Agent Result")
+        st.subheader("👁️ Vision Agent")
         st.write(response.text)
+
+        st.subheader("🌦️ Weather Agent")
+
+        weather_url = "https://api.openweathermap.org/data/2.5/weather"
+
+        params = {
+            "q": city,
+            "appid": st.secrets["WEATHER_API_KEY"],
+            "units": "metric"
+        }
+
+        with st.spinner("🌦️ Checking local weather..."):
+            weather_response = requests.get(
+                weather_url,
+                params=params
+            )
+
+        if weather_response.status_code == 200:
+
+            weather = weather_response.json()
+
+            temperature = weather["main"]["temp"]
+            humidity = weather["main"]["humidity"]
+            description = weather["weather"][0]["description"]
+
+            st.write("📍 Location:", city)
+            st.write("🌡️ Temperature:", temperature, "°C")
+            st.write("💧 Humidity:", humidity, "%")
+            st.write("☁️ Conditions:", description)
+
+        else:
+            st.error("Unable to get weather information.")
